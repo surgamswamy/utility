d1f0252dff0d42084fc3d6b158994d5cdb1f7600
surgamswamy <venu.surgam1@gmail.com>
2023-02-04 11:11:27 +0530

    adding test method

---
+++ src/com/generator/test/GenApp.java
@@ -0,0 +1,14 @@
+package com.generator.test;
+
+public class GenApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new GenApp().getValue());
+     
+	}
+	public double getValue() {
+		return Math.random();
+	}
+     
+}
